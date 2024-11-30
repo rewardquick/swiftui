b9df8da37c1ca640607ce44011eb97dd6d9f630e
Asadbek <72589300+AsadbekNematov@users.noreply.github.com>
2024-11-30 00:45:25 -0500

    Add files via upload

---
+++ FastReward/FastRewardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastRewardApp.swift
+//  FastReward
+//
+//  Created by Asadbek Nematov on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FastRewardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
